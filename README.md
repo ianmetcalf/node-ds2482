2f8a6b43fd6adc89fe0edc5a9a44fd8c96d93015
Ian Metcalf <metcai@gmail.com>
2014-07-23 08:19:17 -0400

    Initial setup for module

---
+++ README.md
@@ -0,0 +1,10 @@
+# DS2482
+
+Provides an interface for the Dallas DS2482 onewire bridge
+
+# Install
+
+```
+$ npm install ds2482
+```
+
---
+++ index.js
@@ -0,0 +1,12 @@
+var I2c = require('i2c');
+
+var DS2482 = function(address, options) {
+  options = options || {};
+
+  this.i2c = new I2c(0x18 | (address & 0x03), {
+    device: options.device || '/dev/i2c-1',
+    debug: options.debug || false
+  });
+};
+
+module.exports = DS2482;
